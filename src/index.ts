a4aa17913e50b6c6a145e72292098aeaee3323c5
simtun <simonas.tunaitis@gmail.com>
2021-09-12 22:00:11 +0300

    Work in Progress

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
-import 'bootstrap';
-import hljs from 'highlight.js';
+import prismjs from 'prismjs';
+import { default as hljs } from 'highlight.js';
 
+import './styles/main.scss';
 
-const code = document.querySelector('#code') as HTMLTextAreaElement;
+const codeInput = document.querySelector('#code') as HTMLTextAreaElement;
+const generatedCodeInput = document.querySelector('#generatedCode') as HTMLTextAreaElement;
 const preview = document.querySelector('#preview') as HTMLElement;
-const languagesDropdown = document.querySelector('#languages .dropdown-menu') as HTMLElement;
+const languageInput = document.querySelector('#languages') as HTMLInputElement;
+const themeInput = document.querySelector('#themes') as HTMLInputElement;
+const autoDetectLanguageInput = document.querySelector('#autoDetectLanguage') as HTMLInputElement;
+const languageStyleLink = document.querySelector('#language-style') as HTMLLinkElement;
 
-code.addEventListener('input', (event) => {
-    const output = hljs.highlight(code.value, { language: 'html' });
-    console.log(output);
+const languages = ['html'];
+const themes = ['default', 'github', 'monokai', 'rainbow'];
+
+const updatePreview = () => {
+    hljs.configure({ cssSelector: 'code' });
+    const output = hljs.highlight(codeInput.value, { language: 'html' });
     preview.innerHTML = output.value;
+    generatedCodeInput.value = output.value;
+};
+
+codeInput.addEventListener('input', (event) => {
+    updatePreview();
 });
 
-hljs.listLanguages().forEach(lang => {
+console.log('xxx');
 
-    const node = new DOMParser()
-        .parseFromString(`<li><a class="dropdown-item" href="#">${lang}</a></li>`, 'text/html').body.firstElementChild;
-    languagesDropdown.appendChild(node);
+autoDetectLanguageInput.addEventListener('change', (event) => {
+    languageInput.disabled = autoDetectLanguageInput.checked === true;
+});
+
+themeInput.addEventListener('change', (event) => {
+    languageStyleLink.href = `/lib/styles/${themeInput.value}.css`;
+});
+
+languages.forEach(lang => {
+    languageInput.insertAdjacentHTML('beforeend', `<option value="${lang}">${lang}</option>`);
+})
+
+themes.forEach(theme => {
+    themeInput.insertAdjacentHTML('beforeend', `<option value="${theme}">${theme}</option>`);
 });
 
 
+updatePreview();
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
